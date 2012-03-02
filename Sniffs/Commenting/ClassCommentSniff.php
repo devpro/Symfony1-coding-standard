053b43111a292a5ba70c937d03d4326acbb1b2b4
Bertrand THOMAS <bertrand@devpro.fr>
2012-03-02 13:51:16 +0100

    Changed package and subpackage convention (lower case accepted)

--- Sniffs/Commenting/ClassCommentSniff.php
+++ Sniffs/Commenting/ClassCommentSniff.php
@@ -3,14 +3,8 @@
 /**
  * Parses and verifies the doc comments for classes.
  *
- * PHP version 5
- *
  * @category  PHP
  * @package   PHP_CodeSniffer
- * @author    Greg Sherwood <gsherwood@squiz.net>
- * @author    Marc McIntyre <mmcintyre@squiz.net>
- * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
 
@@ -40,11 +34,6 @@ if (class_exists('Symfony_Sniffs_Commenting_FileCommentSniff', true) === false) 
  *
  * @category  PHP
  * @package   PHP_CodeSniffer
- * @author    Greg Sherwood <gsherwood@squiz.net>
- * @author    Marc McIntyre <mmcintyre@squiz.net>
- * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
- * @version   Release: 1.3.0
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
 class Symfony_Sniffs_Commenting_ClassCommentSniff extends PEAR_Sniffs_Commenting_FileCommentSniff
@@ -59,10 +48,13 @@ class Symfony_Sniffs_Commenting_ClassCommentSniff extends PEAR_Sniffs_Commenting
         $this->tags['link'    ]['required'] = false;
     }
 
+
+    // ----------------------------------------------------
+    // PEAR_Sniffs_Commenting_FileCommentSniff functions
+
     /**
-     * Returns an array of tokens this test wants to listen for.
-     *
-     * @return array
+     * (non-PHPdoc)
+     * @see PEAR_Sniffs_Commenting_FileCommentSniff::register()
      */
     public function register()
     {
@@ -74,13 +66,8 @@ class Symfony_Sniffs_Commenting_ClassCommentSniff extends PEAR_Sniffs_Commenting
     }
 
     /**
-     * Processes this test, when one of its tokens is encountered.
-     *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token
-     *                                        in the stack passed in $tokens.
-     *
-     * @return void
+     * (non-PHPdoc)
+     * @see PEAR_Sniffs_Commenting_FileCommentSniff::process()
      */
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
@@ -210,11 +197,8 @@ class Symfony_Sniffs_Commenting_ClassCommentSniff extends PEAR_Sniffs_Commenting
     }
 
     /**
-     * Process the version tag.
-     *
-     * @param int $errorPos The line number where the error occurs.
-     *
-     * @return void
+     * (non-PHPdoc)
+     * @see PEAR_Sniffs_Commenting_FileCommentSniff::processVersion()
      */
     protected function processVersion($errorPos)
     {
@@ -226,6 +210,62 @@ class Symfony_Sniffs_Commenting_ClassCommentSniff extends PEAR_Sniffs_Commenting
                 $error = 'Content missing for @version tag in doc comment';
                 $this->currentFile->addWarning($error, $errorPos, 'EmptyVersion');
             }
+        }
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see PEAR_Sniffs_Commenting_FileCommentSniff::processPackage()
+     */
+    protected function processPackage($errorPos)
+    {
+        $this->checkPackage($this->commentParser->getPackage(), $errorPos);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see PEAR_Sniffs_Commenting_FileCommentSniff::processSubpackage()
+     */
+    protected function processSubpackage($errorPos)
+    {
+        $this->checkPackage($this->commentParser->getSubpackage(), $errorPos);
+    }
+
+
+    // ----------------------------------------------------
+    // Internal functions
+
+    /**
+     *
+     * @param unknown_type $package
+     * @param int $package
+     */
+    protected function checkPackage($package, $errorPos, $isSub = false)
+    {
+        if ($package !== null) {
+            $content = $package->getContent();
+            if ($content !== '') {
+//                 if (PHP_CodeSniffer::isUnderscoreName($content) !== true) {
+//                     $newContent = str_replace(' ', '_', $content);
+//                     $nameBits   = explode('_', $newContent);
+//                     $firstBit   = array_shift($nameBits);
+//                     $newName    = strtoupper($firstBit{0}).substr($firstBit, 1).'_';
+//                     foreach ($nameBits as $bit) {
+//                         $newName .= strtoupper($bit{0}).substr($bit, 1).'_';
+//                     }
+
+//                     $error     = ($isSub ? 'Subpackage' : 'Package').' name "%s" is not valid; consider "%s" instead';
+//                     $validName = trim($newName, '_');
+//                     $data      = array(
+//                         $content,
+//                         $validName,
+//                     );
+//                     $this->currentFile->addError($error, $errorPos, $isSub ? 'InvalidSubpackage' : 'InvalidPackage', $data);
+//                   }
+              } else {
+                $error = ($isSub ? '@subpackage' : '@package').' tag must contain a name';
+                $this->currentFile->addError($error, $errorPos, $isSub ? 'EmptySubpackage' : 'EmptyPackage');
+              }
         }
     }
 }
